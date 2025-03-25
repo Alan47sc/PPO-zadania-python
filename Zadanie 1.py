4986e90b4f2457eb8f9d4e581a19b09cf2df4e6c
Alan <aakawa47@gmail.com>
2025-03-25 17:43:16 +0100

    zadanie pierwsze

---
+++ Zadanie 1.py
@@ -0,0 +1,24 @@
+liczba1 = float(input("Podaj pierwszą liczbę: "))
+liczba2 = float(input("Podaj drugą liczbę: "))
+
+print("""Co chcesz zrobić z podanymi liczbami:
+ 1. Dodać
+ 2. Odjąć
+ 3. Pomnożyć
+ 4. Podzielić """)
+
+wybor = int(input())
+
+if wybor == 1:
+    print(liczba1, " + ", liczba2, " = ", liczba1+liczba2)
+elif wybor == 2:
+    print(liczba1, " - ", liczba2, " = ", liczba1-liczba2)
+elif wybor == 3:
+    print(liczba1, " * ", liczba2, " = ", liczba1*liczba2)
+elif wybor == 4:
+    print(liczba1, " / ", liczba2, " = ", liczba1/liczba2)
+else:
+    print("Nieprawidłowy wybór.")
+
+
+
