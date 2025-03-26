cf2159568f5ed486aa9eaa072b6472de07499575
Alan <aakawa47@gmail.com>
2025-03-26 11:19:54 +0100

    Zadanie drugie, przelicznik stopni

---
+++ Zadanie 2.py
@@ -0,0 +1,15 @@
+wybor = input("""Wybierz rodzaj konwersji: 
+C - Stopnie Celsjusza na Fahrenheita
+F - Stopnie Fahrenheita na Celsjusza
+: """)
+
+if wybor == "C" or wybor == "c":
+    celsjusz = float(input("Podaj temperaturę w skali Celsjusza: "))
+    print(celsjusz, "°C to", celsjusz * 1.8 + 32, "°F.")
+
+elif wybor == "F" or wybor == "f":
+    fahren = float(input("Podaj temperaturę w skali Fahrenheita: "))
+    print (fahren, "°F to",(fahren - 32)/1.8, "°C.")
+
+else:
+    print("Nieprawidłowy wybór.")
