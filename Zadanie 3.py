d55574e76a35c6c787119da07e3a961d76556ec2
Alan <aakawa47@gmail.com>
2025-03-26 12:18:32 +0100

    Dodanie zadania 3 - kalkulator średniej ocen ucznia

---
+++ Zadanie 3.py
@@ -0,0 +1,20 @@
+n = int(input("Podaj liczbę ocen do wprowadzenia: "))
+oceny = []
+
+
+for i in range(n):
+    ocena = float(input("Podaj kolejno oceny: "))
+    oceny.append(ocena)
+
+srednia = sum(oceny)/n
+
+if  3 <= srednia <= 6:
+    print("średnia ocen ucznia:", sum(oceny)/n)
+    print("Uczeń zdał")
+
+elif srednia < 3:
+    print("Średnia ocen ucznia:", sum(oceny)/n)
+    print("Uczeń nie zdał.")
+
+else:
+    print("Niepoprawne dane.")
